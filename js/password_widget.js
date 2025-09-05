f26742318afc70fedc631bf2875a427334b2ede6
KoalaNation <92378590+dsigmabcn@users.noreply.github.com>
2025-09-05 20:33:21 +0200

    update password widget

--- js/password_widget.js
+++ js/password_widget.js
@@ -1,18 +1,37 @@
 import { app } from "../../scripts/app.js";
 
 app.registerExtension({
-    name: "your.node.password",
+    name: "your.downloader.password",
     async beforeRegisterNodeDef(nodeType, nodeData, app) {
-        if (nodeData.name === "YourNodeName") { // Replace with your actual node name
+        // Handle CivitAI Downloader
+        if (nodeData.name === "CivitAIDownloader") {
             const onNodeCreated = nodeType.prototype.onNodeCreated;
             nodeType.prototype.onNodeCreated = function () {
                 const r = onNodeCreated ? onNodeCreated.apply(this, arguments) : undefined;
                 
-                // Find the token widget and make it a password field
-                const tokenWidget = this.widgets.find(w => w.name === "token");
-                if (tokenWidget && tokenWidget.inputEl) {
-                    tokenWidget.inputEl.type = "password";
-                }
+                setTimeout(() => {
+                    const tokenWidget = this.widgets?.find(w => w.name === "token_id");
+                    if (tokenWidget?.inputEl) {
+                        tokenWidget.inputEl.type = "password";
+                    }
+                }, 100);
+                
+                return r;
+            };
+        }
+        
+        // Handle HF Downloader
+        if (nodeData.name === "HFDownloader") {
+            const onNodeCreated = nodeType.prototype.onNodeCreated;
+            nodeType.prototype.onNodeCreated = function () {
+                const r = onNodeCreated ? onNodeCreated.apply(this, arguments) : undefined;
+                
+                setTimeout(() => {
+                    const hfTokenWidget = this.widgets?.find(w => w.name === "hf_token");
+                    if (hfTokenWidget?.inputEl) {
+                        hfTokenWidget.inputEl.type = "password";
+                    }
+                }, 100);
                 
                 return r;
             };
